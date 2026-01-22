6ba024c79850a4f791f258d704194dd359c89986
gayas5 <gayasuddin541999@gmail.com>
2026-01-22 18:46:37 +0530

    Delete vars directory

--- vars/mavenBuild.groovy
+++
@@ -1,9 +0,0 @@
-def call(String dir = '.') {
-    echo "Running Maven build in directory: ${dir}"
-
-    dir(dir) {
-        withMaven(maven: 'maven-3') {
-            sh 'mvn clean package'
-        }
-    }
-}
